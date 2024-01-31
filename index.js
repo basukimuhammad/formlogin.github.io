af59448c94fb8f6e0a99b796926145c736628086
basukimuhammad <116169432+basukimuhammad@users.noreply.github.com>
2024-01-31 22:09:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,457 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+// Konfigurasi transporter untuk mengirim email menggunakan Gmail
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'codeotpverifikasi@gmail.com',
+    pass: 'zyti splg fqkd lgto'
+  }
+});
+
+// Menggunakan middleware body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/srcdoc', (req, res) => {
+  res.redirect('/');
+});
+
+// Menampilkan halaman login
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f2f2f2;
+          }
+          
+          .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+            text-align: center;
+            max-width: 400px;
+            width: 100%;
+          }
+          
+          .container h2 {
+            color: #333;
+            margin-bottom: 20px;
+          }
+          
+          .container label {
+            display: block;
+            margin-bottom: 10px;
+            color: #333;
+            font-weight: bold;
+            text-align: left;
+          }
+          
+          .container input[type="text"],
+          .container input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            margin-bottom: 20px;
+          }
+          
+          .container input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .container button {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .container button:hover {
+            background-color: #555;
+          }
+          
+          @media (min-width: 768px) {
+            body {
+              height: auto;
+            }
+            
+            .container {
+              max-width: 400px;
+            }
+          }
+        </style>
+      </head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <body>
+        <div class="container">
+          <h2>Login</h2>
+          <form action="/" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" value="Login">
+          </form>
+          <br>
+          <button onclick="location.href='/register'">Daftar</button>
+        </div>
+      </body>
+    </html>
+  `);
+});
+app.post('/', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Mengecek apakah username dan password sesuai dengan yang ada di datauser.json
+  const data = fs.readFileSync('datauser.json');
+  const users = JSON.parse(data);
+  const user = users.find(user => user.username === username && user.password === password);
+  if (!user) {
+    res.send('Username atau password salah. Silakan coba lagi.');
+    return;
+  }
+
+  // Mengecek apakah pengguna sudah diverifikasi
+  if (!user.verified) {
+    res.send('Akun Anda belum diverifikasi. Silakan verifikasi terlebih dahulu.');
+    return;
+  }
+
+  res.redirect('https://7999bacc-9b21-42c0-9e60-e5a3b41a3290-00-nxo1oxhcqzu0.sisko.replit.dev/');
+});
+
+// Menampilkan halaman daftar
+app.get('/register', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f2f2f2;
+          }
+          
+          .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+            text-align: center;
+            max-width: 400px;
+            width: 100%;
+          }
+          
+          .container h2 {
+            color: #333;
+            margin-bottom: 20px;
+          }
+          
+          .container label {
+            display: block;
+            margin-bottom: 10px;
+            color: #333;
+            font-weight: bold;
+            text-align: left;
+          }
+          
+          .container input[type="text"],
+          .container input[type="email"],
+          .container input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            margin-bottom: 20px;
+          }
+          
+          .container input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .container button {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .container button:hover {
+            background-color: #555;
+          }
+        </style>
+        <script>
+        // Fungsi untuk memutar audio secara otomatis
+        function playAudio() {
+          var audio = new Audio('./audio.mp3');
+          audio.play();
+        }
+
+        // Memanggil fungsi playAudio saat halaman selesai dimuat
+        window.onload = playAudio;
+        </script>
+      </head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <body>
+        <div class="container">
+          <h2 style="color: #333;">Daftar</h2>
+          <form action="/register" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" value="Daftar" style="background-color: #333; color: #fff; padding: 10px 20px; border: none; cursor: pointer;">
+          </form>
+          <br>
+          <button onclick="location.href='/'" style="background-color: #333; color: #fff; padding: 10px 20px; border: none; cursor: pointer;">Kembali ke Login</button>
+        </div>
+      </body>
+    </html>
+  `);
+});
+
+// Mengirimkan kode OTP melalui email
+app.post('/register', (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Cek apakah username sudah ada yang menggunakan
+  const data = fs.readFileSync('datauser.json');
+  let users = [];
+  if (data.length !== 0) {
+    users = JSON.parse(data);
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+      res.send('Username sudah digunakan. Silakan gunakan username lain.');
+      return;
+    }
+  }
+
+  // Cek apakah email sudah ada yang menggunakan
+  const existingEmail = users.find(user => user.email === email);
+  if (existingEmail) {
+    res.send('Email sudah digunakan. Silakan gunakan email lain.');
+    return;
+  }
+
+  // Generate kode OTP acak
+  const otp = Math.floor(100000 + Math.random() * 900000);
+
+  // Konfigurasi email yang akan dikirim
+  const mailOptions = {
+    from: 'codeotpverifikasi@gmail.com',
+    to: email,
+    subject: 'Kode OTP untuk Verifikasi',
+    text: `Kode OTP Anda adalah: ${otp}`
+  };
+
+  // Mengirim email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('Terjadi kesalahan saat mengirim email.');
+    } else {
+      console.log('Email terkirim: ' + info.response);
+
+      // Menyimpan data pengguna ke file JSON
+      const newUser = {
+        username: username,
+        password: password,
+        email: email,
+        otp: otp,
+        verified: false
+      };
+      users.push(newUser);
+      fs.writeFileSync('datauser.json', JSON.stringify(users));
+
+      res.redirect('/verify'); // Mengarahkan pengguna ke halaman verifikasi
+    }
+  });
+});
+
+// Menampilkan halaman verifikasi kode OTP
+app.get('/verify', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f2f2f2;
+          }
+          
+          .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+            text-align: center;
+            max-width: 400px;
+            width: 100%;
+          }
+          
+          .container h2 {
+            color: #333;
+            margin-bottom: 20px;
+          }
+          
+          .container label {
+            display: block;
+            margin-bottom: 10px;
+            color: #333;
+            font-weight: bold;
+            text-align: left;
+          }
+          
+          .container input[type="text"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            margin-bottom: 20px;
+          }
+          
+          .container input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .container button {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .container button:hover {
+            background-color: #555;
+          }
+        </style>
+      </head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <body>
+        <div class="container">
+          <h2 style="color: #333;">Verifikasi Kode OTP</h2>
+          <form action="/verify" method="post">
+            <label for="otp">Kode OTP:</label>
+            <input type="text" id="otp" name="otp" required><br><br>
+            <input type="submit" value="Verifikasi" style="background-color: #333; color: #fff; padding: 10px 20px; border: none; cursor: pointer;">
+          </form>
+        </div>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/verify', (req, res) => {
+  const enteredOtp = req.body.otp;
+
+  // Mengecek apakah kode OTP yang dimasukkan sesuai dengan yang dikirimkan
+  const data = fs.readFileSync('datauser.json');
+  const users = JSON.parse(data);
+  const user = users.find(user => user.otp === parseInt(enteredOtp));
+  if (!user) {
+    res.send('Kode OTP tidak valid. Silakan coba lagi.');
+    return;
+  }
+
+  // Mengupdate status verifikasi pengguna
+  user.verified = true;
+  fs.writeFileSync('datauser.json', JSON.stringify(users));
+  res.send(`
+    <html>
+      <head>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f2f2f2;
+          }
+          
+          .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+            text-align: center;
+            max-width: 400px;
+            width: 100%;
+          }
+          
+          .container h2 {
+            color: #333;
+            margin-bottom: 20px;
+          }
+          
+          .container p {
+            color: #333;
+            margin-bottom: 20px;
+          }
+          
+          .container button {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          .container button:hover {
+            background-color: #555;
+          }
+        </style>
+      </head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <body>
+        <div class="container">
+          <h2 style="color: #333;">Verifikasi Berhasil</h2>
+          <p>Akun Anda telah berhasil diverifikasi.</p>
+          <button onclick="location.href='/'" style="background-color: #333; color: #fff; padding: 10px 20px; border: none; cursor: pointer;">Kembali ke Login</button>
+        </div>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
